c5251ff4f46e6fc31b7dbc701bf69625aa8903d0
hammadahmad120 <hammad.shahid120@gmail.com>
2024-05-12 17:48:13 +0500

    updated logging

--- helpers/logging.interceptor.ts
+++ helpers/logging.interceptor.ts
@@ -10,17 +10,32 @@ export class LoggingInterceptor implements NestInterceptor {
     ) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request: Request = context.switchToHttp().getRequest();
+    const maskedRequestBody = this.maskSecureProperties(request.body, ["password"]);
     return next
       .handle()
       .pipe(
         tap((data) => {
-            this.loggerService.log('info', `url: ${request.url} ${request["user"]?.email ? ` -- user:${request["user"]?.email}`:'' } -- Response:   %o`, data);
+            this.loggerService.log('info', `${request.method} ${request.url} ${request["user"]?.email ? ` -- user:${request["user"]?.email}`:'' } `);
+            this.loggerService.log('info', ` Request body:   %o`, maskedRequestBody);
+            this.loggerService.log('info', `Response:   %o`, data);
         }),
       ).pipe(
         catchError(err => {
-            this.loggerService.log('error', `url: ${request.url} ${request["user"]?.email ? ` -- user: ${request["user"]?.email}`:'' }  -- Error Response:  %o`, err.response);
-            throw err
+          this.loggerService.log('error', `${request.method} ${request.url} ${request["user"]?.email ? ` -- user:${request["user"]?.email}`:'' } `);
+          this.loggerService.log('error', `Request body:   %o`, maskedRequestBody);
+          this.loggerService.log('error', `Error Response:   %o`,  err.response);
+          throw err
         }),
       );
+  }
+  private maskSecureProperties(requestBody: ReadableStream<Uint8Array> | null, toEmptyProperties: [string] )  {
+
+    const objectCopy = {...(requestBody || {})}
+    for (const [key] of Object.entries(objectCopy)) {
+      if(toEmptyProperties.includes(key)){
+        objectCopy[key] = null;
+      }
+  }
+  return objectCopy;
   }
 }
